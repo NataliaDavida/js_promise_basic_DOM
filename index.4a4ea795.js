8322f1c126383f85dc4d4c68832a0fafa0017d69
NataliaDavida <nataliadavida88@gmail.com>
2024-09-09 18:40:42 +0300

    make build

---
+++ index.4a4ea795.js
@@ -0,0 +1,2 @@
+var e=new Promise(function(e,t){document.querySelector(".logo").addEventListener("click",function(){e()})}),t=new Promise(function(e,t){setTimeout(function(){t(Error("Something went wrong"))},3e3)});function n(){var e=document.createElement("div");e.classList.add("message"),e.textContent="Promise was resolved!",document.body.appendChild(e)}function o(){var e=document.createElement("div");e.classList.add("error-message"),e.textContent="Promise was rejected!",document.body.appendChild(e)}e.then(n).catch(o),t.then(n).catch(o);
+//# sourceMappingURL=index.4a4ea795.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,27 @@
+"use strict";
+const promise1 = new Promise((resolve, reject)=>{
+    document.querySelector(".logo").addEventListener("click", ()=>{
+        resolve();
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        reject(new Error("Something went wrong"));
+    }, 3000);
+});
+function handleSuccess() {
+    const successMessage = document.createElement("div");
+    successMessage.classList.add("message");
+    successMessage.textContent = "Promise was resolved!";
+    document.body.appendChild(successMessage);
+}
+function handleError() {
+    const errorMessage = document.createElement("div");
+    errorMessage.classList.add("error-message");
+    errorMessage.textContent = "Promise was rejected!";
+    document.body.appendChild(errorMessage);
+}
+promise1.then(handleSuccess).catch(handleError);
+promise2.then(handleSuccess).catch(handleError);
+
+//# sourceMappingURL=index.f75de5e1.js.map
